cf91155bdcd3033b696886f2c4477c4a0847615d
tavisca-akadam <ankadam@tavisca.com>
2019-08-14 17:43:09 +0530

    Set IP address from program.cs

--- SimpleWebAPI/Program.cs
+++ SimpleWebAPI/Program.cs
@@ -19,6 +19,7 @@ namespace SimpleWebAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls(urls:"http://*:57801");
     }
 }
