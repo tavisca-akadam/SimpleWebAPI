ea2ad657a06f3bffa314f1177b300575281cfd7c
tavisca-akadam <ankadam@tavisca.com>
2019-08-14 12:11:37 +0530

    Added Message Controller and GET request Done

---
+++ SimpleWebAPI/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        // GET 
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Hello", "User" };
+        }
+        // GET api/values
+        [HttpGet("{message}")]
+        public ActionResult<string> Get(string message)
+        {
+            if (message == "hello")
+                return "hi";
+            else if (message == "hi")
+                return "hello";
+            return "Invalid token";
+        }
+    }
+}
